984e4dcf258b628d8326d6acc81cac959f9387d0
Hackbright Student <user@mini-006.(none)>
2013-10-17 12:44:25 -0700

    displays booklist on web

--- bookList.js
+++ bookList.js
@@ -13,6 +13,9 @@ var BookDatabase = function(username){
     this.booksNotRead = [];
 
     this.addBook = function(bookTitle, genre, author, isRead, readDate){
+        if (readDate){
+            readDate = new Date(readDate[0], readDate[1]-1, readDate[2]);
+        }
         var book = new Book(bookTitle, genre, author, isRead, readDate);
         if (isRead === true){
             this.booksRead.push(book);
@@ -25,11 +28,11 @@ var BookDatabase = function(username){
 
     this.currentBook = function(){
         this.currentBook.isRead = true;
-        return this.booksNotRead[this.booksNotRead.length - 1];
+        return this.booksNotRead[0];
     };
     
     this.nextBook = function(){
-        return this.booksNotRead[this.booksNotRead.length - 2];
+        return this.booksNotRead[1];
     };
     this.lastBook = function(){
         return this.booksRead[this.booksRead.length -1];
@@ -41,28 +44,47 @@ var BookDatabase = function(username){
     this.finishCurrentBook = function(){
         this.currentBook.readDate = new Date();
         this.booksRead.push(this.currentBook());
-        this.booksNotRead.pop();
+        this.booksNotRead.shift();
 
     };
     
 };
 
 
+var displayBooks = function(){
+    var pageNode = document.getElementsByTagName('body')[0];
+    var newUl = document.createElement('ul');
+    pageNode.appendChild(newUl);
+    for (i = 0; i < megsBooks.bookShelf().length; i++){
+        var newLi = document.createElement('li');
+        var bookText = document.createTextNode(megsBooks.bookShelf()[i].bookTitle + ", " + megsBooks.bookShelf()[i].author);
+        newLi.appendChild(bookText);
+        newUl.appendChild(newLi);
+    }
+};
 
 
 var megsBooks = new BookDatabase('Meghan');
 
 megsBooks.addBook("The Magus", "coming of age", "John Fowles");
-megsBooks.addBook("Harry Potter", "fantasy", "Jk Rowling", true, new Date(2002, 6, 30));
-megsBooks.addBook("All the Truth in Me", "Young Adult", false, "Angela Burns");
+megsBooks.addBook("Harry Potter", "fantasy", "Jk Rowling", true, [2002, 6, 30]);
+megsBooks.addBook("All the Truth in Me", "Young Adult", "Angela Burns");
+megsBooks.addBook("Don't let's go to the dogs tonight", "memoir", "Alexandra Fowler", true, [2008, 10, 30]);
+megsBooks.addBook("Out of Africa", "memoir", "Isak Dinesen", false);
 
-console.log("Meg's current book: ");
-console.log(megsBooks.currentBook());
+// console.log("Meg's current book: ");
+// console.log(megsBooks.currentBook());
 
-console.log(megsBooks.finishCurrentBook());
-console.log("Books Read: ");
-console.log(megsBooks.booksRead);
-console.log("Books not Read: ");
-console.log(megsBooks.booksNotRead);
-console.log("Meg's new current book");
-console.log(megsBooks.currentBook());
+// console.log(megsBooks.finishCurrentBook());
+// console.log("Books Read: ");
+// console.log(megsBooks.booksRead);
+// console.log("Books not Read: ");
+// console.log(megsBooks.booksNotRead);
+// console.log("Meg's new current book");
+// console.log(megsBooks.currentBook());
+
+displayBooks();
+
+
+
+
